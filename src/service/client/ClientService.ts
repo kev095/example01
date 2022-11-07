19cb8344cb4d461fe193b7fdfe78a6a917c40d4c
KevinJoseph <kevramos95@gmail.com>
2022-11-07 13:26:57 -0500

    fix clientservice

--- src/service/client/ClientService.ts
+++ src/service/client/ClientService.ts
@@ -1,2 +1,15 @@
+import { ClientRepository } from "../../repository/client/ClientRepository";
+import { ClientRepositoryInterface } from "../../repository/client/ClientRepositoryInterface";
 import { Client } from "../../schema/Client";
 import { ClientServiceInterface } from "./ClientServiceInterface";
+
+export class ClientService implements ClientServiceInterface {
+  async saveClient(client: Client): Promise<boolean> {
+    const repository: ClientRepositoryInterface = new ClientRepository();
+    return await repository.save(client);
+  }
+
+  getClient(id: string): Promise<Client> {
+    throw console.error();
+  }
+}
